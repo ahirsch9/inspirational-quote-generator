f69ae1ef8b6239d9a6d0769dd4623e557e5d1cec
ahirsch9 <51544766+ahirsch9@users.noreply.github.com>
2019-06-17 10:46:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,4 @@ $(document).ready(function() {
     $("#quoteBody").fadeOut("slow").fadeIn("slow");
   });
 
-  //function to fetch and show the quote
-  function fetchQuote(q, a) {
-    var quoteNum = Math.floor(Math.random() * quotes.length);
-    $("#quoteBody").text('" ' + q[quoteNum] + ' "');
-    $("#authorBody").text("-" + a[quoteNum]);
-    $(".tweet-btn").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + q[quoteNum] + '."' + "by - " + a[quoteNum]));
-  }
-});
+  
