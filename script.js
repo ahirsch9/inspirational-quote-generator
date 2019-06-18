3a981cee3ff540e794f823f20b7b19e009a8c43d
ahirsch9 <51544766+ahirsch9@users.noreply.github.com>
2019-06-17 21:25:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
-  var quotes = ["Everything is fair in love and war", "life is beautiful be grateful", "Hello World is the program everyone should write", "Hi world is program every girl should write"];
-  var author = ["Ram Shrestha", "Hari Chalise", "Gopal Joshi", "Subhendu Bhata"];
+  var quotes = ["Be the change that you wish to see in the world", "Keep your face to the sunshine and you cannot see a shadow", "A champion is defined not by their wins but by how they can recover when they fall", "Happiness is not by chance, but by choice", "If you're always trying to be normal you will never know how amazing you can be"];
+  var author = ["Mahatma Gandhi", "Helen Keller", "Serena Williams", "Jim Rohn", "Maya Angelou"];
   //to show default quote when starting a page
   fetchQuote(quotes, author);
   //Shows next quote when button is clicked
-  $("#nxtQuote").click(function() 
+  $("#nxtQuote").click(function() {
+    fetchQuote(quotes, author);
+    $gs=quotes&text=' + encodeURIComponent('"' + q[quoteNum] + '."' + "by - " + a[quoteNum]));
+  }
+});
 
   
