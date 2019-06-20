e702b8d4a630a9d72dd086726956f18c2e9fe4cd
ahirsch9 <51544766+ahirsch9@users.noreply.github.com>
2019-06-20 14:01:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-var= "be the change you wish to see in the world" 
+$(document).ready(function() {
 
+  var quotes = ["Be the change that you wish to see in the world", "Keep your face to the sunshine and you cannot see a shadow", "A champion is defined not by their wins but by how they can recover when they fall", "Happiness is not by chance, but by choice", "If you're always trying to be normal you will never know how amazing you can be"];
+  var author = ["Mahatma Gandhi", "Helen Keller", "Serena Williams", "Jim Rohn", "Maya Angelou"];
+ //to show default quote when starting a page
+  fetchQuote(quotes, author);
+  //Shows next quote when button is clicked
+  $("#nxtQuote").click(function() {
+    fetchQuote(quotes, author);
+    $("#quoteBody").fadeOut("slow").fadeIn("slow");
+  });
+
+  //function to fetch and show the quote
+  function fetchQuote(q, a) {
+    var quoteNum = Math.floor(Math.random() * quotes.length);
+    $("#quoteBody").text('" ' + q[quoteNum] + ' "');
+    $("#authorBody").text("-" + a[quoteNum]);
+  }
+});
   
